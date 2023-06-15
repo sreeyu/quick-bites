f9089e09f4238682ad5551f70b8680d33835de88
sreey <sreelakshmi12898@gmail.com>
2023-06-15 22:44:17 +0530

    Display menu items

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment} from "react";
 import MenuSummary from "./MenuSummary";
+import MenuItems from "./MenuItems";
 
 function Menu(){
     return(
         <Fragment>
             <MenuSummary />
+            <MenuItems />
         </Fragment>
     );
 };
