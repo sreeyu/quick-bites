cab113e3aca50b1902ee0dd8e129991967a3c5db
sreey <sreelakshmi12898@gmail.com>
2023-06-16 19:39:13 +0530

    Add styling to meal items

--- src/components/Menu/Meals/Meals.jsx
+++ src/components/Menu/Meals/Meals.jsx
@@ -3,12 +3,14 @@ import styles from './Meals.module.css'
 
 function Meals(props){
 
+    const price = `₹${props.price.toFixed(2)}`
+
     return(
-        <li>
+        <li className={styles.meals} >
             <div>
                 <h3>{props.name}</h3>
-                <div>{props.description}</div>
-                <div>{props.price}</div>
+                <div className={styles.description} >{props.description}</div>
+                <div className={styles.price} >{price}</div>
             </div>
             
         </li>
