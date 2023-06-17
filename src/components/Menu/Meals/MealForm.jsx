38311394c5ff4d568d06de0d2208fc090b6af7c5
sreey <sreelakshmi12898@gmail.com>
2023-06-17 20:31:32 +0530

    Add input field to take the number of each item

--- src/components/Menu/Meals/MealForm.jsx
+++ src/components/Menu/Meals/MealForm.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from './MealForm.module.css'
+import Input from "../../UI/Input";
 
 function MealForm(){
     return(
         <form className={styles.form} >
+            <Input label={"Amount"} input={{id: 'amount', type: 'number', min: '1', max: '5', step: '1' }} />
             <button>+ ADD</button>
         </form>
     )
