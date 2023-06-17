0fbf2a00bf924dd63cc762acf7293de9b13bcb0f
sreey <sreelakshmi12898@gmail.com>
2023-06-17 20:40:30 +0530

    Add styling to input component

--- src/components/Menu/Meals/MealForm.jsx
+++ src/components/Menu/Meals/MealForm.jsx
@@ -5,7 +5,7 @@ import Input from "../../UI/Input";
 function MealForm(){
     return(
         <form className={styles.form} >
-            <Input label={"Amount"} input={{id: 'amount', type: 'number', min: '1', max: '5', step: '1' }} />
+            <Input label={"Amount"} input={{id: 'amount', type: 'number', min: '1', max: '5', step: '1', defaultValue: '1' }} />
             <button>+ ADD</button>
         </form>
     )
