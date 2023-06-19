81107fb4a07ad9e18449e2f28079ef8ac3474d85
sreey <sreelakshmi12898@gmail.com>
2023-06-19 14:29:42 +0530

    Add Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/MainPage/Header';
 import Menu from './components/Menu/Menu';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
 
@@ -17,13 +18,13 @@ function App() {
   }
   
   return (
-    <Fragment >
+    <CartProvider >
       {showCart && <Cart onCancel={getCancelCart} />}
       <Header onCart={getCartModal} />
       <main>
         <Menu />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
