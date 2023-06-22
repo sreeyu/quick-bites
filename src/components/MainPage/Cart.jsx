0ccb52d7283081c38af927374a0117a04286cf3c
sreey <sreelakshmi12898@gmail.com>
2023-06-22 12:41:43 +0530

    Add animation to cart while adding items to the cart

--- src/components/MainPage/Cart.jsx
+++ src/components/MainPage/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from './Cart.module.css'
 import CartIcon from '../Cart/CartIcon'
 import CartContext from '../../store/cart-context';
@@ -10,9 +10,31 @@ function Cart(props){
     const itemNumber = ctx.items.reduce((sum, item) => {
         return sum + Number(item.amount);
     },0)
+
+    const [bumpAnimation, setBumpAnimation] = useState(false)
+
+    const { items } = ctx;
+
+    useEffect(() => {
+        
+        if(items.length === 0){
+            return;
+        }
+        setBumpAnimation(true)
+
+        const timer = setTimeout(() => {
+            setBumpAnimation(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items]);
+
+    const btnClass = `${styles.cart} ${bumpAnimation ? styles.bump : ''}`
     
     return(
-        <button className={styles.cart} onClick={props.onCart} >
+        <button className={btnClass} onClick={props.onCart} >
             <span className={styles.icon} >
                 <CartIcon />
             </span>
