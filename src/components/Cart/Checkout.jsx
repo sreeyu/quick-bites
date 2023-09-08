a20d01ceb70955091aac47cbe3ee9d3921d291f7
sreey <sreelakshmi12898@gmail.com>
2023-09-08 14:36:24 +0530

    Styling changes

--- src/components/Cart/Checkout.jsx
+++ src/components/Cart/Checkout.jsx
@@ -5,25 +5,27 @@ import styles from './Checkout.module.css';
 function Checkout(props){
 
     return(
-        <form>
-            <div>
+        <form className={styles.form}>
+            <div className={styles.control} >
                 <label htmlFor="name">Your Name</label>
                 <input type="text" id="name" />
             </div>
-            <div>
+            <div className={styles.control} >
                 <label htmlFor="street">Street</label>
                 <input type="text" id="street" />
             </div>
-            <div>
+            <div className={styles.control} >
                 <label htmlFor="city">City</label>
                 <input type="text" id="city" />
             </div>
-            <div>
+            <div className={styles.control} >
                 <label htmlFor="postal">Postal Code</label>
                 <input type="text" id="postal" />
             </div>
-            <button type="button" onClick={props.onCancel} >Cancel</button>
-            <button>Confirm</button>
+            <div className={styles.actions} >
+                <button type="button" onClick={props.onCancel} >Cancel</button>
+                <button className={styles.submit} >Confirm</button>
+            </div>
         </form>
     );
 };
